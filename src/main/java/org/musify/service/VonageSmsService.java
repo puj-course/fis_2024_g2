6fe8e629447dc53a028a43438b0e92c305c8c829
Valkyrie <val3kta@gmail.com>
2024-10-27 19:59:36 -0500

    solucion conflicto misc.xml

--- src/main/java/org/musify/service/VonageSmsService.java
+++ src/main/java/org/musify/service/VonageSmsService.java
@@ -1,23 +1,29 @@
 package org.musify.service;
+
 import com.vonage.client.VonageClient;
 import com.vonage.client.sms.MessageStatus;
 import com.vonage.client.sms.SmsSubmissionResponse;
-import com.vonage.client.sms.messages.TextMessage;;
+import com.vonage.client.sms.messages.TextMessage;
 
 public class VonageSmsService {
-    private final VonageClient client = VonageClient.builder().apiKey("8ed53cef").apiSecret("piteAJkuZ9cShMZl").build();
+
+    private final VonageClient client;
+
+    public VonageSmsService() {
+        String apiKey = System.getenv("VONAGE_API_KEY");
+        String apiSecret = System.getenv("VONAGE_API_SECRET");
+
+        if (apiKey == null || apiSecret == null) {
+            throw new IllegalArgumentException("Vonage API credentials not set in environment variables");
+        }
+
+        this.client = VonageClient.builder().apiKey(apiKey).apiSecret(apiSecret).build();
+    }
 
     public boolean enviarSMS(String numeroDestino, String mensaje) {
-        TextMessage message = new TextMessage("Musify",
-                numeroDestino,
-                mensaje
-        );
+        TextMessage message = new TextMessage("Musify", numeroDestino, mensaje);
         SmsSubmissionResponse response = client.getSmsClient().submitMessage(message);
 
-        if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
-            return true;
-        } else {
-            return false;
-        }
+        return response.getMessages().get(0).getStatus() == MessageStatus.OK;
     }
 }
