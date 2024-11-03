0de0bcf636df851259b23282ffec109b74764d4e
Julian Ramos <julianc.ramos21@gmail.com>
2024-11-02 23:32:11 -0500

    Creacion de la clase para tabla intermedia cancionesXAlbum. Creacion de la clase para id compuesto en la tabla intermedia

--- src/main/java/org/musify/model/CancionesXAlbum.java
+++ src/main/java/org/musify/model/CancionesXAlbum.java
@@ -1,2 +1,28 @@
-package org.musify.model;public class CancionesXAlbum {
+package org.musify.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cancionesXAlbum")
+public class CancionesXAlbum {
+    @EmbeddedId
+    private CancionesXAlbumId id;  // Clave primaria compuesta
+
+    @ManyToOne
+    @MapsId("cancionId")  // Mapea el campo cancionId en CancionAlbumId
+    @JoinColumn(name = "cancion_id_cancion")  // Nombre de columna en la tabla
+    private Cancion cancion;
+
+    @ManyToOne
+    @MapsId("albumId")  // Mapea el campo albumId en CancionAlbumId
+    @JoinColumn(name = "album_id_album")  // Nombre de columna en la tabla
+    private Album album;
 }
