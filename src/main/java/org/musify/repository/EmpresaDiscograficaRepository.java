ed20afacff0b2a8e0da8db9826a0751cac64a2b3
Julian Ramos <julianc.ramos21@gmail.com>
2024-11-03 00:34:17 -0500

    Creacion de empresa discografica

---
+++ src/main/java/org/musify/repository/EmpresaDiscograficaRepository.java
@@ -0,0 +1,7 @@
+package org.musify.repository;
+
+import org.musify.model.EmpresaDiscografica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaDiscograficaRepository extends JpaRepository<EmpresaDiscografica, String> {
+}
