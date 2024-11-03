733d7ab7acde834df3d2b32c994158ec1e1f41a4
Julian Ramos <julianc.ramos21@gmail.com>
2024-11-02 23:27:43 -0500

    Modificacion en la clase album

--- src/main/java/org/musify/model/Album.java
+++ src/main/java/org/musify/model/Album.java
@@ -1,120 +1,33 @@
 package org.musify.model;
 
 import jakarta.persistence.*;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "album")
 public class Album {
 
     @Id
-    @Column(name = "id_album")
+    @Column(name = "id_album", length = 32)
     private String idAlbum;
 
-    @Column(name = "nombre", nullable = false)
+    @Column(nullable = false, length = 30)
     private String nombre;
 
     @Column(name = "fecha_lanzamiento", nullable = false)
+    @Temporal(TemporalType.DATE)
     private Date fechaLanzamiento;
 
-    @Column(name = "imagenurl")
+    @Column(name = "imagenurl", length = 255)
     private String imagenUrl;
-
-    // Opción 1: Usando List
-    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Cancion> canciones = new ArrayList<>();
-
-    // Opción 2: Usando Map con número de pista como key
-    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @MapKey(name = "numeroPista")  // Asumiendo que agregarás este campo a la clase Cancion
-    private Map<Integer, Cancion> cancionesPorPista = new HashMap<>();
-
-    // Constructor vacío
-    public Album() {
-    }
-
-    // Constructor con parámetros básicos
-    public Album(String idAlbum, String nombre, Date fechaLanzamiento, String imagenUrl) {
-        this.idAlbum = idAlbum;
-        this.nombre = nombre;
-        this.fechaLanzamiento = fechaLanzamiento;
-        this.imagenUrl = imagenUrl;
-    }
-
-    // Getters y Setters
-    public String getIdAlbum() {
-        return idAlbum;
-    }
-
-    public void setIdAlbum(String idAlbum) {
-        this.idAlbum = idAlbum;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Date getFechaLanzamiento() {
-        return fechaLanzamiento;
-    }
-
-    public void setFechaLanzamiento(Date fechaLanzamiento) {
-        this.fechaLanzamiento = fechaLanzamiento;
-    }
-
-    public String getImagenUrl() {
-        return imagenUrl;
-    }
-
-    public void setImagenUrl(String imagenUrl) {
-        this.imagenUrl = imagenUrl;
-    }
-
-    // Métodos para List
-    public List<Cancion> getCanciones() {
-        return canciones;
-    }
-
-    public void setCanciones(List<Cancion> canciones) {
-        this.canciones = canciones;
-    }
-
-    public void addCancion(Cancion cancion) {
-        canciones.add(cancion);
-        cancion.setAlbum(this);
-    }
-
-    public void removeCancion(Cancion cancion) {
-        canciones.remove(cancion);
-        cancion.setAlbum(null);
-    }
-
-    // Métodos para Map
-    public Map<Integer, Cancion> getCancionesPorPista() {
-        return cancionesPorPista;
-    }
-
-    public void setCancionesPorPista(Map<Integer, Cancion> cancionesPorPista) {
-        this.cancionesPorPista = cancionesPorPista;
-    }
-
-    public void addCancionConPista(Integer numeroPista, Cancion cancion) {
-        cancionesPorPista.put(numeroPista, cancion);
-        cancion.setAlbum(this);
-    }
-
-    public void removeCancionPorPista(Integer numeroPista) {
-        Cancion cancion = cancionesPorPista.remove(numeroPista);
-        if (cancion != null) {
-            cancion.setAlbum(null);
-        }
-    }
 }
---
+++ src/main/java/org/musify/model/CancionesXAlbum.java
@@ -0,0 +1,2 @@
+package org.musify.model;public class CancionesXAlbum {
+}
