14d806ff8ee74e57b48b0f546759b5dc4a9c5398
Julian Ramos <julianc.ramos21@gmail.com>
2024-11-04 15:36:40 -0500

    Cambio en la ubicación del método getCancionesByAlbum: pasa al controlador correspondiente

--- src/main/java/org/musify/controller/CancionController.java
+++ src/main/java/org/musify/controller/CancionController.java
@@ -1,13 +1,9 @@
 package org.musify.controller;
 
-import jakarta.persistence.EntityNotFoundException;
-import org.musify.model.album.AlbumDTO;
-import org.musify.model.artistasXCancion.ArtistasXCancion;
+
 import org.musify.model.cancion.Cancion;
 import org.musify.model.cancion.CancionDTO;
-import org.musify.service.ArtistasXCancionServiceImpl;
 import org.musify.service.AudioService;
-import org.musify.service.CancionesXAlbumServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +19,6 @@ public class CancionController {
     @Autowired
     private AudioService audioService;
 
-    @Autowired
-    private CancionesXAlbumServiceImpl cancionesXAlbumService;
 
     @PostMapping("/crearCancion")
     public ResponseEntity<?> postCancion(@RequestBody CancionDTO cancionDTO) {
@@ -54,21 +48,5 @@ public class CancionController {
         }
         return ResponseEntity.ok(canciones);
     }
-    @GetMapping("/por-album")
-    public ResponseEntity<?> getCancionesByAlbum(@RequestParam String nombreAlbum) {
-        try {
-            List<CancionDTO> canciones = cancionesXAlbumService.getCancionesByNombreAlbum(nombreAlbum);
 
-            if (canciones == null || canciones.isEmpty()) {
-                return ResponseEntity.notFound().build(); // Se retorna 404 si no hay canciones
-            }
-
-            return ResponseEntity.ok(canciones);
-
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Álbum no encontrado: " + e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
-        }
-    }
 }
