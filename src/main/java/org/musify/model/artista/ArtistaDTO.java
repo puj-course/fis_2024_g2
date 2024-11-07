2b2160c1f1a648aab43fc883759f58abb11efbae
Julian Ramos <julianc.ramos21@gmail.com>
2024-11-06 20:07:28 -0500

    Cambio en artista: ahora tambien tiene una foto de perfil como atributo

--- src/main/java/org/musify/model/artista/ArtistaDTO.java
+++ src/main/java/org/musify/model/artista/ArtistaDTO.java
@@ -4,17 +4,20 @@ public class ArtistaDTO {
     private String nombreArtistico;
     private String biografia;
     private String usuarioId;
+    private String fotoPerfilUrl;
 
     public ArtistaDTO() {}
-    public ArtistaDTO(String nombreArtistico, String biografia, String usuarioId) {
+    public ArtistaDTO(String nombreArtistico, String biografia, String usuarioId, String fotoPerfilUrl) {
         this.nombreArtistico = nombreArtistico;
         this.biografia = biografia;
         this.usuarioId = usuarioId;
+        this.fotoPerfilUrl = fotoPerfilUrl;
     }
     public ArtistaDTO(Artista a) {
         this.nombreArtistico = a.getNombreArtistico();
         this.biografia = a.getBiografia();
         this.usuarioId = a.getIdUsuario();
+        this.fotoPerfilUrl = a.getFotoPerfilUrl();
     }
 
     public String getNombreArtistico() {
@@ -39,5 +42,13 @@ public class ArtistaDTO {
 
     public void setUsuarioId(String usuarioId) {
         this.usuarioId = usuarioId;
+    }
+
+    public String getFotoPerfilUrl() {
+        return fotoPerfilUrl;
+    }
+
+    public void setFotoPerfilUrl(String fotoPerfilUrl) {
+        this.fotoPerfilUrl = fotoPerfilUrl;
     }
 }
